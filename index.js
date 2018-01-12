21263bdadc8a02caed1d900980d1a2d544e2707e
Elodie CHAUVEAU <elochauveau@hotmail.fr>
2018-01-12 12:52:40 +0100

    test

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const myModule = require(./myModule);
-console.log(myModule.foo);
+//const myModule = require(./myModule);
+//console.log(myModule.foo);
 
 //console.log('hello');
+
+function component()
+{
+	let element = document.createElement('div');
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	return element;
+}
+
+document.body.appendChild(component());
