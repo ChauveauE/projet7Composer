09dc9e695616da33796fb09bcb3fc7be093c3e99
Elodie CHAUVEAU <elochauveau@hotmail.fr>
2018-01-16 09:37:24 +0100

    modif webpack.config

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,10 +65,21 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ 
-(function(module, exports) {
+/***/ (function(module, exports) {
 
-console.log('hello');
+//const myModule = require(./myModule);
+//console.log(myModule.foo);
+
+//console.log('hello');
+
+function component()
+{
+	let element = document.createElement('div');
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	return element;
+}
+
+document.body.appendChild(component());
 
 /***/ })
 /******/ ]);
